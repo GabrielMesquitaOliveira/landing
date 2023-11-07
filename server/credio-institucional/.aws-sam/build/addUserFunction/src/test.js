35c602d39f7fcd525b6a33d0d0e3a235a8cb9222
ENXAQEUCAKRONYCA <willspc@gmail.com>
2023-11-07 19:03:59 -0300

    feature / add server

---
+++ server/credio-institucional/.aws-sam/build/addUserFunction/src/test.js
@@ -0,0 +1,25 @@
+// const Loaders = require("../database");
+
+// const { addUser, getAllUsers } = require("../database/users");
+
+// async function save() {
+//   const user = {
+//     name: "Usuario teste De banco",
+//     phone: "(23) 123-123123",
+//     email: "usuario_test_21312312gmail.com",
+//   };
+//   await addUser(user);
+// }
+// async function run() {
+//   console.info("starting database");
+//   await Loaders.start();
+//   await save();
+
+//   const data = await getAllUsers();
+
+//   console.info(" response data : ");
+
+//   console.info(data);
+// }
+
+// run();
--- server/credio-institucional/.aws-sam/build/getAllUsersFunction/database/connection/index.js
+++ server/credio-institucional/.aws-sam/build/getAllUsersFunction/database/connection/index.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const config = require("config");
+console.info(config);
+
 const dbConfig = config.get("PMS.dbConfig.name");
+
+console.info(dbConfig);
 
 async function startDB() {
   mongoose.connect(dbConfig);
